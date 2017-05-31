80db7f9a2f44575a7d8dc44d17e8dbf35fd8fd88
Ecmel Kaytazoğlu <ecmel.kytz@gmail.com>
2017-05-31 16:29:49 +0300

    Use pg as the database

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  default_scope { order( created_at: :asc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170531102647) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
